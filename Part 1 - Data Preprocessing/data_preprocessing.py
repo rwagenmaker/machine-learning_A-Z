9b4c8d7f9b005e9a7ce8aaea252ab34e38a35335
rwagenmaker <[rw1818@gmail.com]>
2017-12-13 13:59:30 +0000

    multilinear regression

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -27,19 +27,22 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 # Encode the categorical variables : 0-France, 1-Germany, 2-Spain
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features=[0])  # the column number of the features that are categorical
+# Generate dummy variables for the categorical possibilities; which in this case are 3 new dummy variables
+onehotencoder = OneHotEncoder(categorical_features=[0])  # the column index of the features that are categorical
 X = onehotencoder.fit_transform(X).toarray()
+
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
-"""
 
+"""
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+"""
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
+
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
@@ -17,14 +18,14 @@ X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features=[3])
 X = onehotencoder.fit_transform(X).toarray()
 
-# Avoiding the Dummy Variable Trap
+# Avoiding the Dummy Variable Trap/ redundant dependancies
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling
+# Feature Scaling | this is not needed because the libraries used do it for us
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -39,3 +40,15 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+###############################################################################
+###############################################################################
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+"""
+this "statsmodels" library does not take into consideration the constant b0, so we need to add an extra column of 1s.
+This means the this extra column would represent the "X0" in the "b0*X0" part of the equation.
+"""
+X = np.append(arr=X, values=np.ones((50, 1)))
+

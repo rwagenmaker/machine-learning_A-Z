2af29fcb3d441e07cd3d2eee785d07b9b9057c8a
rwagenmaker <rw1818@gmail.com>
2017-12-24 19:56:13 +0000

    polynomial regression

--- Part2 - Regression/Section5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part2 - Regression/Section5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -25,7 +25,7 @@ X = X[:, 1:]
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling | this is not needed because the libraries used do it for us
+# Feature Scaling | this is not needed because the libraries we use here, do it for us
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -48,7 +48,36 @@ y_pred = regressor.predict(X_test)
 import statsmodels.formula.api as sm
 """
 this "statsmodels" library does not take into consideration the constant b0, so we need to add an extra column of 1s.
-This means the this extra column would represent the "X0" in the "b0*X0" part of the equation.
+This means that this extra column would represent the "X0" in the "b0*X0" part of the equation.
 """
-X = np.append(arr=X, values=np.ones((50, 1)))
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+np.set_printoptions(precision=1, suppress=True)
+print(X)
+
+
+
+"""
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]  # all possible predictors
+
+# Step 1: select significance level => SL = 0.05 (5%)
+# Step 2: fit the multiple linear regression model to our future optimal matrix of features X_opt & Y
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+# Step 3: remove predictor with the highest P-value
+regressor_OLS.summary()
+"""
+
+num_rows, num_columns = X.shape
+features_indexes = list(range(0, num_columns))
+pvalues = [1]
+while (max(pvalues) > 0.05):
+    X_opt = X[:, features_indexes]  # all possible predictors
+    regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+    regressor_OLS.summary()
+    pvalues = regressor_OLS.pvalues
+    max_pval_index = pvalues.tolist().index(max(pvalues))  # find out the highest P-value index
+    features_indexes.pop(max_pval_index)
+    print(features_indexes)
+    print(max(pvalues))
+
 

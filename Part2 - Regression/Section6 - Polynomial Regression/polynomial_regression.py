8aacdf2d891679c9f1a88721661529eebbc4c685
rwagenmaker <rw1818@gmail.com>
2017-12-27 15:17:22 +0000

    decision tree regression

--- Part2 - Regression/Section6 - Polynomial Regression/polynomial_regression.py
+++ Part2 - Regression/Section6 - Polynomial Regression/polynomial_regression.py
@@ -1,24 +1,39 @@
-# Polynomial Regression
+"""
+Polynomial Regression
+
+-> Build a bluffing detector using regression models, and predict if the
+new employee is bluffing about his previous salary
+
+(new employee says previous salary of 160k)
+"""
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+np.set_printoptions(precision=3, suppress=True)
+
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
+# ||| This is the same as selecting just the index 1 column, since the upper bound is not included.
+# ||| We do it so that python sees X as a matrix and not as a vector
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# Splitting the dataset into the Training set and Test set
+# Splitting the dataset into the Training set and Test set ||| no need to do because the dataset is too small
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
+# |||  No need to do because polynomial regression uses the same library as before,
+# ||| which means that the scaling is automatically handled
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
+
+# -------------------------- End Preprocessing ----------------------------
 
 # Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
@@ -27,23 +42,26 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)  # ||| transform the matrix X into b0, X1 and X1²
 poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
+# ------------------------- End Generating Models ----------------------------
+
 # Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.scatter(X, y, color='red')
+# ||| 2nd arg is the vector containing the predicted salaries
+plt.plot(X, lin_reg.predict(X), color='blue')
 plt.title('Truth or Bluff (Linear Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Polynomial Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -52,8 +70,8 @@ plt.show()
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')

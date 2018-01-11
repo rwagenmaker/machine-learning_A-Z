a1fd72bf00feeac1955b210bcc2a58d2b2ff38a3
rwagenmaker <rw1818@gmail.com>
2018-01-11 10:46:40 +0000

    rest of classification

--- Part3 - Classification/Section14 - SVM/svm.py
+++ Part3 - Classification/Section14 - SVM/svm.py
@@ -1,4 +1,4 @@
-# Classification template
+# Support Vector Machine (SVM)
 
 # Importing the libraries
 import numpy as np
@@ -12,7 +12,10 @@ y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.25,
+                                                    random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -20,16 +23,9 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# ----------------------- End Pre Processing ---------------------------
-
-# Fitting classifier to the Training set
-# Create your classifier here : 3 Steps:
-# 1 - import the library
-# 2 - create the object
-# 3 - fit the object to the training set
-
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear', random_state=0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -39,27 +35,28 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Pretty print matrix
-from tabulate import tabulate
-table = tabulate(cm, tablefmt="fancy_grid")
-print(table)
-#table2 = tabulate(dataset, tablefmt="fancy_grid")
-# ----------------------------------------------------------------------
-
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+X1, X2 = np.meshgrid(
+    np.arange(
+        start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+    np.arange(
+        start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+
+plt.contourf(
+    X1,
+    X2,
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+    alpha=0.75,
+    cmap=ListedColormap(('red', 'green')))
+
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c=ListedColormap(('red', 'green'))(i), label=j)
-
-plt.title('K-NN (Training set)')
+plt.title('SVM (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -68,17 +65,16 @@ plt.show()
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
-
-plt.title('K-NN (Test set)')
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
